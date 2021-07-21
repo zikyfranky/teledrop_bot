6b16f40f6f8b5f78171bb3d5280596db2d5e1f83
Isaac Frank <codingwithisaac@gmail.com>
2021-07-21 18:22:26 +0100

    server/init: get absolute path so as not to break code no matter where it's run frm

--- server/init.py
+++ server/init.py
@@ -1,17 +1,18 @@
+import os
 import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import db
 from db import NAME
 
+file_path = os.path.abspath(__file__)
+dir_path = os.path.dirname(file_path)
+service_file = 'firebase_private_key.json'
+
 # Fetch the service account key JSON file contents
-cred = credentials.Certificate("./firebase_private_key.json")\
+cred = credentials.Certificate('%s\%s' % (dir_path, service_file))
 
-# Initialize the app with a service account, granting admin privileges
-default_app = firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://%s.firebaseio.com'%NAME
-})
-
-# As an admin, the app has access to read and write all data, regradless of Security Rules
-ref = db.reference('restricted_access/secret_document')
-print(ref.get())
+def initialize():
+    # Initialize the app with a service account, granting admin privileges
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://%s.firebaseio.com'%NAME
+    })
 
