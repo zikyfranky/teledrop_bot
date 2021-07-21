de1c07f93ef3e0e57df941e190d352e798fa03c7
Isaac Frank <codingwithisaac@gmail.com>
2021-07-21 15:19:36 +0100

    bot/helper:  remove 'not needed' functions

--- bot/helper.py
+++ bot/helper.py
@@ -1,21 +1,42 @@
+from requests import put, get
+from _secrets import API_HOST
+
 def extract_referral(message:str) -> str:
     ref = message.split()
     r_v = ref[1] if len(ref) > 1 else ""
     return r_v
 
 
-def in_storage(unique_code):
-    # (pseudo-code) Should check if a unique code exists in storage
-    return True
+def increment_referee_count(user_id: str, ref_id: str, ref_obj) -> int:
+    refs: list = ref_obj.refs.val()
+    refCount: int = ref_obj.refCount
+    refCount +=1
+    refs.append(user_id)
+    put(API_HOST + '/' + ref_id, data={'refs': refs, 'refCount':refCount}).json()
+    return refCount
 
+def add_ref(user_id:str, ref_id:str):
+    if user_id == ref_id:
+        raise Exception('You can\'t refer yourself')
 
-def get_username_from_storage(unique_code):
-    # (pseudo-code) Does a query to the storage, retrieving the associated username
-    # Should be replaced by a real database-lookup.
-    return "ABC" if in_storage(unique_code) else None
+    user = get('%s/%s' % (API_HOST, user_id)).json()
+    print(user)
+    user_exists = user.status_code == 200
 
+    if user_exists:
+        user_ref = user.data.ref
+        if not user_ref:
+            ref = get(API_HOST + '/' + ref_id).json()
+            ref_exists = ref.status_code == 200
+            if ref_exists:
+                res = put(API_HOST + '/' + user_id, data={'ref':ref_id}).json()
+                if res.status_code == 200:
+                    print('Updated user referral')
+                    return increment_referee_count(user_id, ref_id, ref.data)
+                else:
+                    print('Error saving referral')
+            else:
+                print('Invalid referral')
+        else:
+            print('User is already referred')
 
-def save_chat_id(chat_id, username):
-    # (pseudo-code) Save the chat_id->username to storage
-    # Should be replaced by a real database query.
-    pass
